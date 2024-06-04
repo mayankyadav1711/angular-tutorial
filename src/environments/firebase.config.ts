79627053820840f0bbb1cb85b5c4f738de3a45f6
mayankyadav1711 <mykyadav2003@gmail.com>
2024-06-04 13:56:48 +0530

    mission in progress

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+// firebase.config.ts
+const firebaseConfig = {
+    apiKey: "Replace",
+    authDomain: "Replace",
+    projectId: "Replace",
+    storageBucket: "Replace",
+    messagingSenderId: "Replace",
+    appId: "Replace",
+    measurementId: "G-Replace"
+  };
---
+++ src/environments/getFirebase.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "Replace",
+    authDomain: "Replace",
+    projectId: "Replace",
+    storageBucket: "Replace",
+    messagingSenderId: "Replace",
+    appId: "Replace",
+    measurementId: "G-Replace"
+  };
+
+export function getFirebaseConfig() {
+  return firebaseConfig;
+}
